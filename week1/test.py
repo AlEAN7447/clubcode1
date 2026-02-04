867b012f03758b2651f09b8bf6e3d78328b7fae4
AlEAN7447 <khuttra422@gmail.com>
2026-02-04 16:33:18 +0700

    random number

---
+++ week1/test.py
@@ -0,0 +1,12 @@
+import random
+
+
+# if var1 == var2 :
+#     print("you are right")
+# else:
+#     print("You are wrong")    
+num = random.randint(1, 10)  # 1 to 10 (inclusive)
+if num == 5 :
+ print("you are right")
+else:
+ print("Try again")
